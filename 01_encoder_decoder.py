afea2a9ec4c8058d39d8b1f26abdc77bcef318ca
Stefan D. Krstić <115481825+Stefan-Krstic@users.noreply.github.com>
2023-02-14 22:06:08 +0100

    Initial commit

---
+++ 01_encoder_decoder.py
@@ -0,0 +1,38 @@
+# This program will shift each letter of your message by the 'shift amount' entered by the user.
+# In the 'encode' mode, it moves in one direction, while in 'decode' mode it reverses shift (to view original message)
+# Note that numbers and symbols will remain unchanged during the process.
+
+print(f"{'-' * 44}\nWelcome to Stefan's [Encoder/Decoder] program!\n{'-' * 44}")
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {cipher_direction}d result:\n{'-' * 30}\n{end_text}\n{'-' * 30}")
+
+should_end = False
+while not should_end:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("End of Program!")
